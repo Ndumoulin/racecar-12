30f0fd898816a08adedd8a3c7ca502dcd3c4985b
Ndumoulin <nathan.dumoulin11@outlook.com>
2025-09-17 15:09:44 -0400

    7 instead of 8.5 V

--- racecar_teleop/racecar_teleop/slash_teleop.py
+++ racecar_teleop/racecar_teleop/slash_teleop.py
@@ -111,19 +111,19 @@ class Teleop(Node):
                 self.cmd_msg.angular.z = 0.0
                 self.cmd_msg.linear.z  = 6.0  # Control mode
                 
+            #If button start is active 
+            elif(joy_msg.buttons[9]):   
+                # Reset Encoder
+                self.cmd_msg.linear.x  = 7.0
+                self.cmd_msg.angular.z = 0.0
+                self.cmd_msg.linear.z  = 9.0  # Control mode
+                
             #If button back is active 
             elif(joy_msg.buttons[8]):   
                 # Reset Encoder
                 self.cmd_msg.linear.x  = 5.0
                 self.cmd_msg.angular.z = 0.0
                 self.cmd_msg.linear.z  = 8.0  # Control mode
-            
-            #If button start is active 
-            elif(joy_msg.buttons[9]):   
-                # Reset Encoder
-                self.cmd_msg.linear.x  = 8.4
-                self.cmd_msg.angular.z = 0.0
-                self.cmd_msg.linear.z  = 9.0  # Control mode
                 
             #If left trigger is active 
             elif (joy_msg.buttons[6]):
